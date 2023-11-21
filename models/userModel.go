5eedf2a87b05500e1d9def357be98b0804b88ee3
eshiettjoseph <eshiett314@gmail.com>
2023-11-21 22:32:24 +0100

    add models/userModel.go

--- models/userModel.go
+++ models/userModel.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	ID string `json:"id"`
+	ID uint `json:"id"`
 	Name string `json:"name"`
-	Email string `json:"email"`
-	Age string `json:"age"`
+	Email *string `json:"email"`
+	Age uint8 `json:"age"`
 }
