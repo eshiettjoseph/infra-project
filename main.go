da2661e191507fc052de90a03a9fcc5a60e0e59a
eshiettjoseph <eshiett314@gmail.com>
2023-11-28 12:22:01 +0100

    add routers

--- main.go
+++ main.go
@@ -1,51 +1,14 @@
 package main
 
 import (
+	"go-rest-api/controllers"
 	"go-rest-api/initializers"
-	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
 // represents data about User data
 
-
-// user slice to seed user data
-// var users = []User{
-// 	{ID: "1", Name: "Joseph Eshiett", Email: "josepheshiett@gmail.com", Age: "22"},
-// 	{ID: "2", Name: "James Smith", Email: "johnsmith@email.com", Age: "20"},
-// 	{ID: "3", Name: "John Doe", Email: "johndoe@email.com", Age: "30"},
-// }
-
 // responds with list of all users
-func getUsers(c *gin.Context){
-	c.IndentedJSON(http.StatusOK, users)
-}
-
-// responds with users added
-func addUser(c *gin.Context){
-	var newUser User
-	if err := c.BindJSON(&newUser); err != nil {
-        return
-    }
-	users = append(users, newUser)
-    c.IndentedJSON(http.StatusCreated, newUser)
-}
-
-// responds with singular user
-func getUser(c *gin.Context){
-	// parameter used for query of data
-	id := c.Param("id")
-
-    // Loop through the list of albums, looking for
-    // an album whose ID value matches the parameter.
-    for _, a := range users {
-        if a.ID == id {
-            c.IndentedJSON(http.StatusOK, a)
-            return
-        }
-    }
-    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
-}
 
 // Run function before main
 func init(){
@@ -56,9 +19,11 @@ func init(){
 }
 func main(){
 	router := gin.Default()
-	router.GET("/api/users", getUsers)
-	router.GET("/api/user/:id", getUser)
-	router.POST("api/user/:id", addUser)
+	router.POST("/api/user/", controllers.AddUser)
+	router.GET("/api/users/", controllers.GetUsers)
+	router.GET("/api/user/:id", controllers.GetUser)
+	router.DELETE("/api/user/:id", controllers.DeleteUser)
+	router.PUT("/api/user/:id", controllers.UpdateUser)
 
 	router.Run()
 }
