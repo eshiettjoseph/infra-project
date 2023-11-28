be9ac9899596121bd09206a10facdf57e817a615
eshiettjoseph <eshiett314@gmail.com>
2023-11-28 16:37:38 +0100

    add backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,25 @@
 terraform {
-    required_version = "~> 1.3"
-    
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.0"
-      }
+  required_version = "~> 1.3"
+
+  backend "s3" {
+    bucket         = "go-rest-tf"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "goRestTf"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
-    
+  }
+
+}
+
+module "tf-state" {
+  source      = "./modules/tf-state"
+  bucket_name = local.bucket_name
+  table_name  = local.table_name
 }
