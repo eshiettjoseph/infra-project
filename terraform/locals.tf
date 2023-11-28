63e9b3fac2e8dabf706d6e270a0783b73dc6ed79
eshiettjoseph <eshiett314@gmail.com>
2023-11-28 18:37:25 +0100

    uncomment local some variables

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,11 +5,11 @@ locals {
     ecr_repo_name = "go-rest-api-ecr-repo"
 
 
-  #   demo_app_cluster_name        = "demo-app-cluster"
-  #   availability_zones           = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  #   demo_app_task_famliy         = "demo-app-task"
-  #   container_port               = 3000
-  #   demo_app_task_name           = "demo-app-task"
+    go_rest_api_cluster_name        = "go-demo-api-cluster"
+    availability_zones           = ["us-east-1a", "us-east-1b", "us-east-1c"]
+    go_rest_api_task_famliy         = "go-rest-api-task"
+    container_port               = 3000
+    go_rest_api_task_name           = "go-rest-api-task"
   #   ecs_task_execution_role_name = "demo-app-task-execution-role"
 
   #   application_load_balancer_name = "cc-demo-app-alb"
