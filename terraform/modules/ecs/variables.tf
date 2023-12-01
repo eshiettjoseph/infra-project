d2d2d36b25271b59cf08f57dd91e6b7213283c4b
eshiettjoseph <eshiett314@gmail.com>
2023-12-01 03:48:13 +0100

    revamp terraform infra

--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -1,49 +1,65 @@
-variable "go_rest_api_cluster_name" {
-  description = "ECS Cluster Name"
-  type        = string
-}
-
 variable "availability_zones" {
   description = "us-east-1 AZs"
   type        = list(string)
 }
 
-variable "go_rest_api_task_famliy" {
-  description = "ECS Task Family"
-  type        = string
+variable "postgres_password_arn" {
+  description = "The postgres password arn"
 }
 
-variable "ecr_repo_url" {
-  description = "ECR Repo URL"
-  type        = string
+variable "postgres_endpoint_arn" {
+  description = "The postgres Endpoint arn"
+}
+
+variable "db_user_name" {
+  description = "the name of the postgres DB user name"
+}
+
+variable "ecs_cluster_name" {
+  description = "ECS Cluster name"
+}
+
+
+variable "autoscaling_policy_name" {
+  description = "AWS App autoscaling policy name"
 }
 
 variable "container_port" {
-  description = "Container Port"
-  type        = number
+  description = "Port number for go-rest-api container"
 }
 
-variable "go_rest_api_task_name" {
-  description = "ECS Task Name"
-  type        = string
+variable "container_name" {
+  description = "Name for go-rest-api container"
 }
 
-variable "ecs_task_execution_role_name" {
-  description = "ECS Task Execution Role Name"
-  type        = string
+variable "ecs_service_name" {
+  description = "ECS service name"
 }
 
-variable "application_load_balancer_name" {
-  description = "ALB Name"
-  type        = string
+variable "aws_log_group_and_stream" {
+  description = "AWS Log Group and Stream"
 }
 
-variable "target_group_name" {
-  description = "ALB Target Group Name"
-  type        = string
+variable "go_rest_api_policy_name" {
+  description = "Go Restful API IAM policy name"
 }
 
-variable "go_rest_api_service_name" {
-  description = "ECS Service Name"
-  type        = string
+variable "rds_access_policy_name" {
+  description = "RDS IAM Access policy name"
+}
+
+variable "go_rest_api_ssm_parameter_policy_name" {
+  description = "Go Restful API SSM parameter policy"
+}
+
+variable "secret_manager_policy_name" {
+  description = "Go Restful API secret manager policy name"
+}
+
+variable "aws_lb_name" {
+  description = "AWS Loadbalancer"
+}
+
+variable "aws_lb_target_group_name" {
+  description = "AWS Loadbalancer target group name"
 }
