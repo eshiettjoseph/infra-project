3ed9bea737d1a340170b5ca013c2634517d60d9a
eshiettjoseph <eshiett314@gmail.com>
2024-01-24 13:07:33 +0100

    reformat

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -1,3 +1,12 @@
+resource "aws_ecr_repository" "go_rest_api_ecr_repo" {
+  name                 = var.ecr_repo_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 module "ecs" {
   source                                = "./modules/ecs"
   aws_lb_name                           = local.aws_lb_name
