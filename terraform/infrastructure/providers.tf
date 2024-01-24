92ea8a4926c2af90cb6c7ee9f9c22969ae2d19dd
eshiettjoseph <eshiett314@gmail.com>
2024-01-24 01:26:46 +0100

    deploy infra

--- terraform/infrastructure/providers.tf
+++ terraform/infrastructure/providers.tf
@@ -1,25 +1,18 @@
 terraform {
   required_version = "~> 1.3"
 
-  backend "s3" {
-    bucket         = "go-rest-tf"
-    key            = "tf-infra/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "go-rest-api-dynamodb-table"
-    encrypt        = true
+  backend "remote" {
+    organization = "joeshiett"
+
+    workspaces {
+      name = "joeshiett"
+    }
   }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
-    }
-  }
-  cloud {
-    organization = "joeshiett"
-
-    workspaces {
-      name = "joeshiett"
     }
   }
 }
